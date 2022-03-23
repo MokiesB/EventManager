2c0841ec8b718554e6ef40c6e2ebdfbf899aa87a
MokiesB <90444262+MokiesB@users.noreply.github.com>
2022-03-23 21:43:05 +0100

    added qr code generator

--- src/dk/MMJT/BLL/BLLManager.java
+++ src/dk/MMJT/BLL/BLLManager.java
@@ -6,14 +6,20 @@ import dk.MMJT.DAL.IDALManager;
 
 public class BLLManager implements IBLLManager {
     private IDALManager dalManager;
+    private QRHelper qrHelper;
 
     public BLLManager() {
         this.dalManager = new DALManager();
+        this.qrHelper = new QRHelper();
     }
 
     @Override
     public User loginUser(String username, String password) {
         return dalManager.loginUser(username, password);
     }
-
+ /*  try {
+            qrHelper.generateQRCodeImage("coolEvent");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
 }
--- src/dk/MMJT/BLL/QRHelper.java
+++ src/dk/MMJT/BLL/QRHelper.java
@@ -1,8 +1,23 @@
 package dk.MMJT.BLL;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+
 public class QRHelper {
+    public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+    public static BufferedImage generateEAN13BarcodeImage(String barcodeText) throws Exception {
+        EAN13Writer barcodeWriter = new EAN13Writer();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, 300, 150);
 
-    //This class is responsible for QR codes and barcodes
-
-    
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
 }
